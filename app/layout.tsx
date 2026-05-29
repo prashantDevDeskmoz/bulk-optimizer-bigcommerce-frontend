2143a1f9dff19cb4367cd9cc9ccd7a6c81834f67
prashantDevDeskmoz <prashantsingh.deskmoz@gmail.com>
2026-05-29 12:51:22 +0530

    initial layout commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import {ToastContainer} from "react-toastify"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className="h-full antialiased">
+      <body
+        className={`min-h-full flex flex-col font-sans ${geistSans.variable} ${geistMono.variable}`}
+      >
+        <Providers>{children}</Providers>
+        <ToastContainer position="top-right" autoClose={3000} />
+      </body>
     </html>
   );
 }
